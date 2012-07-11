569710c051029a583f058e4fc683c4dcf3b50d55
xiaoyu8740 <kevin.minyu@gmail.com>
2012-07-11 10:50:50 +0800

    init commit

---
+++ src/main/java/com/nextmenu/cp/Constants.java
@@ -0,0 +1,5 @@
+package com.nextmenu.cp;
+
+public class Constants {
+	public static final String WEB_APP = "/ssm3";
+}
---
+++ src/main/java/com/nextmenu/cp/interceptor/LoginHandlerInterceptor.java
@@ -0,0 +1,12 @@
+package com.nextmenu.cp.interceptor;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginHandlerInterceptor extends HandlerInterceptorAdapter{
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		return false;
+	}
+}
